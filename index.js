2f2423009631e5a7b81ef7292f0b40fb86dae851
NumiBlue <thekindlyblog@gmail.com>
2022-03-08 06:37:51 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function convertInteger (number) {
+    let num = number;
+    let binary = (num % 2).toString();
+    for (; num > 1; ) {
+        num = parseInt(num / 2);
+        binary =  (num % 2) + (binary);
+    }
+    console.log(binary);
+}
+
